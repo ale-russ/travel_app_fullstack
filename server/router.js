160930c533b77e592212baea4e9948a0b9af457e
ale-russ <agarrussom@gmail.com>
2023-10-13 19:06:57 +0300

    removing bson package from package.json

--- server/router.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = require("express").Router();
-const { login, getUsers, addUser } = require("./auth/auth");
-
-router.route("/login").post(login);
-module.exports = router;
